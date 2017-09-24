2cb47b87dca927866cb9fb82bafc229d1fd26479
Fernando Villanueva <jfvs1@hotmail.com>
2017-09-23 21:09:05 -0500

    home.css and foldelCell modified

--- src/gui/views/FolderCell.java
+++ src/gui/views/FolderCell.java
@@ -2,9 +2,13 @@ package gui.views;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.ListCell;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.control.Alert.AlertType;
 import model.Folder;
 
@@ -16,9 +20,16 @@ public class FolderCell extends ListCell<Folder> {
 
         if (folder == null) { return; }
 
-        Button button = new Button(folder.getName());
+        final ImageView imageView = new ImageView(
+        	      new Image("http://www.iconsdb.com/icons/preview/gray/folder-xxl.png")
+        	    );
+        imageView.setFitHeight(40);
+        imageView.setFitWidth(40);
+        Button button = new Button(folder.getName(),imageView);
         // Add class name to this button to apply styles from the CSS file.
         button.getStyleClass().add("folder");
+        button.setAlignment(Pos.BASELINE_LEFT);
+        button.setId("folder");
         button.setOnAction(new EventHandler<ActionEvent>() {
 
 			public void handle(ActionEvent event) {
