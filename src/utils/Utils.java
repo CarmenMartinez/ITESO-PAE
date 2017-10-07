cf081ba1d8e494292da97a97b20d35f2dcf9a3bf
JulianLz96 <jj_lz96@hotmail.com>
2017-10-07 18:21:17 -0500

    UpdateIn

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -17,7 +17,7 @@ public class Utils {
 
 	public static void createWindow(Stage stage, Object parent, String fxmlLocation, String sceneTitle, Object userData, String cssLocation, Double sceneWidth, Double sceneHeight) {
         try {
-        	String resourceLocation = "resources.UIResources_en_MX";
+        	String resourceLocation = "resources.i18n.UIResources_en_MX";
         	ResourceBundle bundle = ResourceBundle.getBundle(resourceLocation);
         	FXMLLoader fxmlLoader = new FXMLLoader(parent.getClass().getResource(fxmlLocation),bundle);
         	Pane root = (Pane) fxmlLoader.load();
